0ed19a2e374f5c4dc7b0be797953c7b6b4b7e6a4
brb11 <121825808+brb11@users.noreply.github.com>
2025-06-24 22:08:43 +0300

    Updated email

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -4,8 +4,8 @@ import myImage from '../../assets/porf_image.jpg';
 const contacts = [
   {
     label: 'Email',
-    value: 'brhoombasha111@gmail.com',
-    link: 'mailto:brhoombasha111@gmail.com',
+    value: 'ebrahim4biz@gmail.com',
+    link: 'mailto:ebrahim4biz@gmail.com',
     icon: (
       <svg className="w-6 h-6 text-[#2C3E50]" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
         <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
